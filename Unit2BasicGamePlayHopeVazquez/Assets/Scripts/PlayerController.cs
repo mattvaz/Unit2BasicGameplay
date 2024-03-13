08258973c50a0e43258a87c0ef85074704ae5308
mattvaz <146034846+mattvaz@users.noreply.github.com>
2024-03-13 15:27:21 -0700

    added lots.

---
+++ Unit2BasicGamePlayHopeVazquez/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x < -10)
+        
+            transform.position = new Vector3(horizontalInput - 10, transform.position.y, transform.position.z);
+
+        
+    }
+}
