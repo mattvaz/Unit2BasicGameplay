c4c3f9ec1274b6e092dac2ac0f95dbe983863704
mattvaz <146034846+mattvaz@users.noreply.github.com>
2024-03-15 15:27:10 -0700

    Added food.
    Added throwing food.

--- Unit2BasicGamePlayHopeVazquez/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayHopeVazquez/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +19,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // keep the player in bounds
+       
+        if(transform.position.x < xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+       
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        if (transform.position.x < -10)
-        
-            transform.position = new Vector3(horizontalInput - 10, transform.position.y, transform.position.z);
 
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile form the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
