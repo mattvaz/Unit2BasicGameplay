087739d0a14f8dc7d34deedf0705beef30ee40cd
mattvaz <146034846+mattvaz@users.noreply.github.com>
2024-03-21 15:21:23 -0700

    added.
    Many things done.

--- Unit2BasicGamePlayHopeVazquez/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2BasicGamePlayHopeVazquez/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,12 +16,16 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
 
-        if (transform.position.z > topBound) { }
-        Destroy(gameObject); 
-        else if (transform.position.z < lowerBound) { }
-        Destroy(gameObject);
 
 
+        }
     }
 }
--- Unit2BasicGamePlayHopeVazquez/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayHopeVazquez/Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
        // keep the player in bounds
        
-        if(transform.position.x < xRange)
+        if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
